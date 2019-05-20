e705ff5bc22bba8e3139e36146377a9927a56282
William Acevedo <william.acevedo@gmail.com>
2019-05-20 17:33:49 +0200

    commit to merge branch

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,6 @@
 class Booking < ApplicationRecord
+  has_many :reviews
+  belongs_to :user
+  belongs_to :meal
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_140412) do
+ActiveRecord::Schema.define(version: 2019_05_20_141450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "meals", force: :cascade do |t|
     t.string "meal_type"
     t.time "time"
     t.text "topic"
     t.integer "seats"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "review_text"
+    t.integer "stars"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
