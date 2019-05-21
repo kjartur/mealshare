d77c8b49167bcb0eef793a9d7ec7c8b5fba6132a
William Acevedo <william.acevedo@gmail.com>
2019-05-21 11:37:16 +0200

    adds routes when MealsController was genereated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'meals/new'
+  get 'meals/create'
+  get 'meals/edit'
+  get 'meals/update'
+  get 'meals/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
