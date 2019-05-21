13dc5e9049d8e02f9cb23e4ac65bfc8ef8642663
William Acevedo <william.acevedo@gmail.com>
2019-05-21 13:56:01 +0200

    made modified :time to datetime datatype

--- db/migrate/20190520140412_create_meals.rb
+++ db/migrate/20190520140412_create_meals.rb
@@ -2,7 +2,7 @@ class CreateMeals < ActiveRecord::Migration[5.2]
   def change
     create_table :meals do |t|
       t.string :meal_type
-      t.time :time
+      t.datetime :time
       t.text :topic
       t.integer :seats
 
