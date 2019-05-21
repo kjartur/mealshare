1362e0460ada51c3399f1d565f9955ed8c29580b
William Acevedo <william.acevedo@gmail.com>
2019-05-21 12:21:09 +0200

    adjust routes for the meals controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'meals/new'
-  get 'meals/create'
-  get 'meals/edit'
-  get 'meals/update'
-  get 'meals/destroy'
-  devise_for :users
+    devise_for :users
+    resources :meals
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
