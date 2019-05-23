e94626f88b6220125c099b21138a11eabc20584e
Julian Hermann <julian.hermann89@gmail.com>
2019-05-23 12:43:18 +0200

    bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -24,17 +24,17 @@ class BookingsController < ApplicationController
     end
   end
 
-  def confirm
-    @user = current_user
-    @meal = Meal.find(params[:meal_id])
-    @booking = Booking.new(booking_params) # GET THE POST parameters
-    render :new if @booking.invalid? # Return if false
-  end
-
 
   private
 
   def booking_params
     params.require(:booking).permit(:seats)
+  end
+
+  def confirm
+    @user = current_user
+    @meal = Meal.find(params[:meal_id])
+    @booking = Booking.new(booking_params) # GET THE POST parameters
+    render :new if @booking.invalid? # Return if false
   end
 end
