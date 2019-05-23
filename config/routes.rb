d70988e7c09172f0d885ab89d892d15b32c8d406
William Acevedo <william.acevedo@gmail.com>
2019-05-23 17:27:22 +0200

    adds :search to routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
       post 'confirm'
     end
   end
-  get "/search", to: "meals#search"
+  get "/search", to: "meals#search", as: :search
   get "/my_bookings", to: "bookings#my_bookings"
   get "/confirmed", to: "bookings#confirm"
 end
