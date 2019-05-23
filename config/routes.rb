9e2f8bcb1ec13afa89020a855a7ba43f148524ef
William Acevedo <william.acevedo@gmail.com>
2019-05-23 14:26:53 +0200

    adds get "/search", to: "meals#search"

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,11 @@ Rails.application.routes.draw do
   resources :meals do
     resources :bookings, only: [:new, :create]
   end
-<<<<<<< HEAD
-  get "/search", to: "meals#search"
-=======
   resources :bookings do
     collection do
       post 'confirm'
     end
   end
->>>>>>> master
+  get "/search", to: "meals#search"
   get "/my_bookings", to: "bookings#my_bookings"
 end
